44637d05d960a0f3f51436bf49896b222263f831
edusyn1 <infoedusynth@gmail.com>
2025-11-04 21:44:10 +0530

    Delete pwa.js

--- pwa.js
+++
@@ -1,6 +0,0 @@
-/*-- pwa botton js --*/
-let deferredPrompt;window.addEventListener('beforeinstallprompt',(e) =>{e.preventDefault();deferredPrompt = e;document.getElementById('pwaInstallBtn').style.display = 'flex'}
-);const btn = document.getElementById('pwaInstallBtn');btn.style.display = 'none';btn.addEventListener('click',async () =>{if (!deferredPrompt) return;deferredPrompt.prompt();const choiceResult = await deferredPrompt.userChoice;if (choiceResult.outcome === 'accepted'){console.log('User accepted the install prompt')}
-else{console.log('User dismissed the install prompt')}
-deferredPrompt = null;btn.style.display = 'none'}
-);
